3a9a44d16f40c396bc038b56bf9350ce08735343
Tomer Arzuan - Home <tomerarzu@gmail.com>
2021-05-25 22:57:56 +0300

    com

--- run.py
+++ run.py
@@ -9,8 +9,8 @@ def create_tables():
     db.create_all()
 
 
-# if __name__ == "__main__":
-#     app.run(port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
 
 @app.before_first_request
 def create_tables():
